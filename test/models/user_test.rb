687844678078e8374f605d28e567e813299f7e4c
Anand <anandnair96@gmail.com>
2019-06-28 13:37:20 +0530

    advanced login in

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
-
+  
   test "should be valid" do
     assert @user.valid?
   end
@@ -86,6 +86,9 @@ test "email addresses should be unique" do
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 
 
